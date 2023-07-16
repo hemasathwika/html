e16a2733829b70ffd4d3c812026044dbd502787f
hemasathwika <135261406+hemasathwika@users.noreply.github.com>
2023-07-17 00:22:44 +0530

    js assignments

---
+++ JavaScript/assignment3/att1.js
@@ -0,0 +1,11 @@
+/*const element = document.getElementById('my-element');
+element.setAttribute('class', 'my-class');
+element.setAttribute('data-id', '123');*/
+const a= require('a');
+
+const document = a.createDocument('<html></html>');
+const element = document.createElement('div');
+element.setAttribute('class', 'my-class');
+element.setAttribute('data-id', '123');
+
+
