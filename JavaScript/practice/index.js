c5cc7dcc62f612271610fcfd4d3ea8f2876bb4c3
hemasathwika <135261406+hemasathwika@users.noreply.github.com>
2023-08-18 21:20:11 +0530

    js

--- JavaScript/practice/index.js
+++ JavaScript/practice/index.js
@@ -1,9 +1,23 @@
-var x = 7;
+//variables
+//in 2 ways we can declare variables 
+//1st way
 
-function getName(){
-    console.log("namasthe");
-}
+//let age = 22
+//console.log(age)
 
-getName();
-console.log(x);
-console.log(getName);
+//2nd way
+
+//const salary = 20000
+//console.log(salary)
+
+//difference between those 2 ways is
+//we can declare "let" without initializing a value whereas we cannot declare "const" without initializing if we do so we will get typerror
+//example as follows
+
+//let sum
+//sum = 5
+//console.log(sum)
+
+//const salary
+//salary = 2121
+//console.log(salary)
